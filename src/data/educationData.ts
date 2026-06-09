879be312b29f7ab9d7a42cfaf518fb2b2037753c
Boondirek Kanjanapongporn <boondirek1@gmail.com>
2026-06-09 19:47:21 +0100

    update education section

--- src/data/educationData.ts
+++ src/data/educationData.ts
@@ -1,8 +1,8 @@
 export const education = [
   {
     institution: "University of Glasgow",
-    degree: "BSc Software Engineering — Computer Science",
-    honour: "First-Class Honours",
+    degree: "Bachelor of Science (B.Sc.) — Software Engineering",
+    honour: "Honours of the First Class",
     grade: "18.36 / 22.00",
     gradeLabel: "Grade",
     location: "Glasgow, United Kingdom",
@@ -11,12 +11,12 @@ export const education = [
     logoLight: "/assets/logos/universityofglasgow.jpg",
     documents: [
       {
-        label: "Transcript",
-        link: "https://drive.google.com/file/d/1dHa8fHfkdp2OJHchltnhKdkYL8RTRDAm/view?usp=drive_link",
-      },
-      {
         label: "Certificate",
         link: "https://drive.google.com/file/d/1msZn1K0gmAXzPQUVvrWIP0vK4DnPPORZ/view?usp=drive_link",
+      },
+      {
+        label: "Transcript",
+        link: "https://drive.google.com/file/d/1dHa8fHfkdp2OJHchltnhKdkYL8RTRDAm/view?usp=drive_link",
       },
     ],
     bullets: [
@@ -28,22 +28,29 @@ export const education = [
   },
   {
     institution: "King Mongkut's Institute of Technology Ladkrabang",
-    degree: "BEng Computer Engineering — Software Engineer",
-    honour: "First-Class Honours",
-    grade: "3.79 / 4.00",
+    degree: "Bachelor of Engineering (B.Eng.) — Software Engineering",
+    honour: "Honours of the First Class",
+    grade: "3.73 / 4.00",
     gradeLabel: "GPA",
     location: "Bangkok, Thailand",
     date: "Aug 2020 – Aug 2022",
     logoDark: "/assets/logos/kmitl.png",
     logoLight: "/assets/logos/kmitl.png",
     documents: [
-      { label: "Transcript", link: "" },
-      { label: "Certificate", link: "" },
+      {
+        label: "Certificate",
+        link: "https://drive.google.com/file/d/1uvOA5W0Gfj9SpEyQsz49lNAYG5uKqxe5/view?usp=sharing",
+      },
+      {
+        label: "Transcript",
+        link: "https://drive.google.com/file/d/1LyItDcWDcfYdM8ZLz5sex-NGkTNMCDt7/view?usp=sharing",
+      },
     ],
     bullets: [
-      "Python Teacher Assistant for 'Introduction to Programming' course",
-      "Full Stack Developer Intern at MFEC Public Company Limited",
       "Freshman 100% Scholarship award",
+      "Software Quality Management Developer Intern at KASIKORN Business-Technology Group (KBTG) for my second year",
+      "Python Teacher Assistant for 'Introduction to Programming' course",
+      "Full Stack Developer Intern at MFEC Public Company Limited for my first year",
       "1st Runner Up of Amazon AWS Build On, Asean 2021 Hackathon Competition",
       "Popular vote winner of Amazon AWS Design the City of Tomorrow 2021 Hackathon Competition",
     ],
